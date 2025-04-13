c24cf51297a2ac6002a50e8bce858c8aeefdbc7b
Kumiho <enotmos@yandex.ru>
2025-04-13 18:25:06 +0300

    Changed the login form style. Changed inputs and a button margins

--- frontend/src/Components/LoginForm.jsx
+++ frontend/src/Components/LoginForm.jsx
@@ -14,17 +14,17 @@ const LoginForm = () => {
     {({ isSubmitting }) => (
       <Container>
         <Form>
-          <Row>
+          <Row className='mb-3'>
             <label  className="form-label" htmlFor="login">Login</label>
-            <Field type="text" name="login" id="login" />
+            <Field className="form-control" type="text" name="login" id="login" />
             <ErrorMessage name="text" component="div" />
           </Row>
-          <Row>
+          <Row className='mb-3'>
             <label className="form-label" htmlFor="password">Password</label>
-            <Field type="password" name="password" id="password" />
+            <Field className="form-control" type="password" name="password" id="password" />
             <ErrorMessage name="password" component="div" />
           </Row>
-          <Row>
+          <Row className='mt-3'>
             <Button variant='primary' type="submit" disabled={isSubmitting}>Submit</Button>
           </Row>
         </Form>
